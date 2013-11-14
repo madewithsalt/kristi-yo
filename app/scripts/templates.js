4eb38364dde2f81ca8dd01a0d40dfc3a9804c41d
Kristi Centinaro <kcent@Kristis-MacBook-Pro.local>
2013-11-14 12:46:44 -0800

    initial setup

---
+++ app/scripts/templates.js
@@ -0,0 +1,10 @@
+this["JST"] = this["JST"] || {};
+
+this["JST"]["app/templates/test.hbs"] = Handlebars.template(function (Handlebars,depth0,helpers,partials,data) {
+  this.compilerInfo = [4,'>= 1.0.0'];
+helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
+  
+
+
+  return "<h1>Test!</h1>";
+  });
